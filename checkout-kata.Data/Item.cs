ed377122362d2537ed471e1c70877c83480c8fb4
Saw Naw <waterchan@gmail.com>
2020-02-02 20:39:08 +0000

    Add Item class

---
+++ checkout-kata.Data/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace checkout_kata.Data
+{
+    public class Item
+    {
+        public string Sku { get; set; }
+        public double UnitPrice { get; set; } 
+
+        /// <summary>
+        /// The quantity that qualifies an item for a special offer price.
+        /// </summary>
+        public int QuantityNeededForSpecialOffer { get; set; }
+
+        /// <summary>
+        /// The special offer price available upon purchasing a certain quantity of an item.
+        /// </summary>
+        public int SpecialOfferPrice { get; set; }
+
+        public Item(string sku, double unitPrice, int specialOfferQuantity, int specialOfferPrice)
+        {
+            this.Sku = sku;
+            this.UnitPrice = unitPrice;
+            this.QuantityNeededForSpecialOffer = specialOfferQuantity;
+            this.SpecialOfferPrice = specialOfferPrice;
+        }
+    }
+}
