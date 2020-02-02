0f90a17bede00c295ead513ea3292a1f51f811a8
Saw Naw <waterchan@gmail.com>
2020-02-02 20:44:16 +0000

    Add the list of available products, as an in-memory List for now.

---
+++ checkout-kata.Data/IProductData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace checkout_kata.Data
+{
+    public interface IProductData
+    {
+        List<Item> GetAllProductData();
+    }
+}
---
+++ checkout-kata.Data/ItemInformation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace checkout_kata.Data
+{
+    public class ItemInformation : IProductData
+    {
+        /// <summary>
+        /// Gets a list of all items available from the data source.
+        /// </summary>
+        /// <remarks>
+        /// In production-ready code, the data source would be a database. 
+        /// For now we'll just simulate it in memory.
+        /// </remarks>
+        public List<Item> GetAllProductData()
+        {
+            List<Item> allItems = new List<Item>()
+            {
+                new Item("A99", 50, 3, 130),
+                new Item("B15", 30, 2, 45),
+                new Item("C40", 60, 0, 0),
+                new Item("T34", 99, 0, 0),
+            };
+
+            return allItems;
+
+        }
+    }
+}
